5969697a879b0b0acfa4afcc1625fa15712c7227
Arvid <arvidberends@hotmail.com>
2015-10-27 13:35:39 +0100

    XML Parser

--- klad/Klad - Arvid.py
+++ klad/Klad - Arvid.py
@@ -1,3 +1,16 @@
+#Methode 1
+import xmltodict
+
+def verwerk_xml():
+    bestand = open('test.xml', 'r')
+    xml_string = bestand.read()
+    return xmltodict.parse(xml_string)
+
+stations_dict = verwerk_xml()
+print(stations_dict['catalog'],['book'],['author'])
+
+
+#Methode 2
 import xml.etree.ElementTree as etree
 
 xmlD = etree.parse("test.xml")
