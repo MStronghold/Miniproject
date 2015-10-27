405dbcaa2d41ce887b92fdce1bc1c3a10dc6af48
Arvid <arvidberends@hotmail.com>
2015-10-27 13:05:38 +0100

    Test3

--- klad/Klad - Arvid.py
+++ klad/Klad - Arvid.py
@@ -1,1 +1,9 @@
-print('Test3')
+import xml.etree.ElementTree as etree
+
+xmlD = etree.parse("test.xml")
+root = xmlD.getroot()
+
+for child in root:
+    for childeren in child:
+        print(childeren.text)
+    print("\n")
--- klad/Klad - Lucas.py
+++ klad/Klad - Lucas.py
@@ -12,7 +12,6 @@ def docstringtest():
 
 
 
-
 url = urllib.request.urlopen('http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=b7w5qxk8dqtvbntnx5uns434fhww6878&dag=27-10-2015&sorteer=0')
 bestand = open('lucasklad', 'a')
 bestand.write(str(url))
