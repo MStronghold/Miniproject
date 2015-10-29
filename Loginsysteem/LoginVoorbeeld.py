5ac5ee5f2d5f6084b1b695132ced895274896248
Johan de Graaf <johan@mcpvp.eu>
2015-10-29 11:26:03 +0100

    Voorbeeld verplaatsen en wat comments toegevoegt aan BezoekerInfo

--- Loginsysteem/LoginVoorbeeld.py
+++ Loginsysteem/LoginVoorbeeld.py
@@ -2,7 +2,7 @@ from Loginsysteem import BezoekerInfo
 from Loginsysteem import Login
 
 # Nieuwe gebruiker aanmaken
-g = BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email1", "Wachtwoord1")
+g = BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email1", "Wachtwoord1", True) # Laatste argument geeft aan of de gebruiker een aanbieder is. True = aanbieder, False = Gewone gebruiker. Dit argument is optioneel en standaard False.
 print(g.to_dict())
 
 # Gebruiker proberen op te slaan in database
@@ -19,11 +19,11 @@ print(Login.Login.gebruiker_opvragen("Gebruiker1").get_email())
 print(Login.Login.gebruiker_opvragen("Email1").get_bezoeker_ID())
 
 # Gebruiker proberen op te slaan en checken of de gebruikersnaam al in gebruik is.
-if Login.Login.gebruiker_opslaan(BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email2", "Wachtwoord2")) == 1:
+if Login.Login.gebruiker_opslaan(BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email2", "Wachtwoord2", False)) == 1:
     print("Gebruikersnaam al in gebruik")
 
 # ~~ en checken of het emailadres al gebruikt is
-if Login.Login.gebruiker_opslaan(BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email2", "Wachtwoord2")) == 2:
+if Login.Login.gebruiker_opslaan(BezoekerInfo.BezoekerInfo.nieuw_bezoeker_rnd("Gebruiker1", "Email2", "Wachtwoord2", False)) == 2:
     print("Emailadres al in gebruik")
 
 # Gebruiker verwijderen uit de database
@@ -33,5 +33,6 @@ Login.Login.gebruiker_verwijderen(g.get_bezoeker_id())
 print(g.to_dict())
 g.set_gebruikersnaam("MijnGebruikersnaam")
 g.set_wachtwoord("MijnWachtwoord")
+g.set_is_aanbieder(True)
 print(g.to_dict())
 Login.Login.gebruiker_opslaan(g, True)
