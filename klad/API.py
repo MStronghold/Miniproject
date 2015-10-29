1061539edd2c2765cf7be7514098b9b16da06cda
MStronghold <deathisafriend93>
2015-10-29 21:49:38 +0100

    V1.0 API.py
    functies samengevoegd tot één geheel. Comments toegevoegd, docstring bijgewerkt, printmethoden behouden in comments voor debugging.
    
    To do: Implementatie TKinter
    
    To do if time > deadline: responsfilter toevoegen, als titel niet gevonden wordt, wordt API argument s= in de url toegevoegd en opnieuw gedraaid om zoekresultaten te weergeven.

--- klad/API.py
+++ klad/API.py
@@ -1,49 +1,60 @@
 """
-Lucas: Bevat alle functies voor interactie met OMDB API.
+Volledige API afhandeling.
 
-Functies:
+-Lucas, 29-10-15
 
 """
-from urllib.request import urlopen
-from ast import literal_eval
+from urllib.request import urlopen      #http connector
+from urllib import error                #error handling
+from ast import literal_eval            #dict naar string converter
+import re                               #regular expressions voor urlopbouw-filtering
 
-def urlhandler(x):
+
+def APIrequest(x):
     """
     Invoerfunctie voor zoekveld.
-    Accepteert string(titel of idnummer)
-    en returned API url naar APIrequest():.
+    Accepteert string(titel of idnummer),
+    vraagt info en returned een dict met informatie.
     :param x:
     :return:
     """
 
+    # vervangt spaties in zoektermen met '+' tekens
+    x = str(x)
+    x = re.sub(r'\s+', '+', x)
+
+    #bouwt API-geaccepteerde url
     url = 'http://www.omdbapi.com/?'
     if x[:2] == 'tt':
         url += 'i=' + x
     else:
         url += 't=' + x
-    APIrequest(url)
 
-
-def APIrequest(url):
+    #verbindt met API en extraheert gegevens, exceptions ingebouwd voor eventueel verbindingsverlies,
+    # server- en client-side
     try:
         with urlopen(url) as f:
             data = f.read().decode()
-        print(data, '\n', '--', '\n')
-        outputhandler(data)
-    except:
-        print(url)
-        print('verbindingsfout', '\n')
-        urlhandler()
+        # # weergave raw data(debug)
+        # print(data, '\n', '--', '\n')
+    except error.HTTPError as H:
+        print('server kan niet aan het verzoek voldoen.')
+        print('Foutcode: ', H.code)
+    except error.URLError as U:
+        print('kan server niet bereiken.')
+        print('Foutcode: ', U.code)
 
 
-def outputhandler(x):
-    gegevens = literal_eval(x)
-    for key in gegevens:
-        print(key, gegevens[key], '\n')
+    #bouwt dictobject
+    gegevens = literal_eval(data)
+    #weergave dict(debug)
+    # for key in gegevens:
+    #     print(key, gegevens[key], '\n')
+    return gegevens
 
 
 
-
-
+#handmatige input(debug)
+APIrequest('robin hood')
 
 
