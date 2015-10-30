014921260a4d47fe74b739436a32ecea02624d17
Johan de Graaf <johan@mcpvp.eu>
2015-10-30 15:14:20 +0100

    Begin aan main, en wat kleine veranderingen aan LoginGui.

--- Main.py
+++ Main.py
@@ -1,1 +1,10 @@
+from klad import LoginGui
 
+# Open LoginGui
+_LoginGui_lst = LoginGui.Login_Frame.open_interface()
+_LoginGui_root = _LoginGui_lst[0]
+_LoginGui_class = _LoginGui_lst[1]
+_LoginGui_root.mainloop()
+
+_ingelogde_gebruiker = _LoginGui_class.gebruiker
+print(_ingelogde_gebruiker.get_gebruikersnaam())
