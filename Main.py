eb5e21e9c42ade1b6711333cbc64d3a2daeb1906
Johan de Graaf <johan@mcpvp.eu>
2015-10-30 16:24:47 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,7 @@
 from LoginInterface import LoginGui
+from MainInterface import StartScherm
+
+# --- LoginGui deel ---
 
 # Open LoginGui
 _LoginGui_lst = LoginGui.Login_Frame.open_interface()
@@ -6,6 +9,14 @@ _LoginGui_root = _LoginGui_lst[0]
 _LoginGui_class = _LoginGui_lst[1]
 _LoginGui_root.mainloop()
 
+# Haal de gebruiker op uit LoginGui
 _ingelogde_gebruiker = _LoginGui_class.gebruiker
-print(_ingelogde_gebruiker.get_gebruikersnaam())
 
+
+# --- StartScherm deel ---
+
+# Open StartScherm
+_StartScherm_lst = StartScherm.StartScherm.open_interface(_ingelogde_gebruiker)
+_StartScherm_root = _StartScherm_lst[0]
+_StartScherm_class = _StartScherm_lst[1]
+_StartScherm_root.mainloop()
