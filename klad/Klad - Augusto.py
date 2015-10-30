e0c2abac4ff3c8322d237eb3499845ba9b96febd
Augusto <gewoonaugusto@hotmail.nl>
2015-10-30 09:31:57 +0100

    startscherm

--- klad/Klad - Augusto.py
+++ klad/Klad - Augusto.py
@@ -1,48 +1,78 @@
 from tkinter import *
+import tkinter.messagebox as bericht
+from Toegangskaarten import KaartDatabase
+import uuid
 
-def startscherm():
+
+root = Tk()
+
+
+class startscherm:
     
-    def klik():
-        print("Klikken werkt!")
-    
-    root = Tk()
-    
-    
-    menu = Menu(root)
-    root.config(menu=menu)
-    
-    # ***** Main Menu *****
-    
-    fileMenu = Menu(menu)
-    menu.add_cascade(label="File", menu=fileMenu)
-    fileMenu.add_command(label="New Project...", command=klik)
-    fileMenu.add_separator()
-    fileMenu.add_command(label="Exit", command=exit)
-    
-    
-    
-    
-    # ***** Toolbar *****
-    
-    toolbar = Frame(root, bg="#A5110D")
-    
-    bestelButton = Button(toolbar, text="Film bestellen", command=klik)
-    bestelButton.pack(side=LEFT, padx=5, pady=10)
-    bekijkButton = Button(toolbar, text="Film bekijken", command=klik)
-    bekijkButton.pack(side=LEFT, padx=5, pady=10)
-    
-    toolbar.pack(side=TOP, fill=X)
-    
-    # ***** Status Bar *****
-    
-    status = Label(root, text="Statusbar...", bd=1, relief=SUNKEN, anchor=W)
-    status.pack(side=BOTTOM, fill=X)
-    
+    def code_checken(self):
+        self.label_1 = Label(root, text="Voer Uw toegangscode in:")
+        self.entry_1 = Entry(root)
+
+        self.label_1.pack(side=TOP)
+        self.entry_1.pack(side=TOP)
+
+        self.button_1 = Button(root, text="Voer in", command=self.klik)
+        self.button_1.pack(side=TOP, pady=4)
+
+    def klik(self):
+        toegangscode = self.entry_1.get()
+
+        code = KaartDatabase.KaartDatabase.kaart_opvragen(toegangscode)
+
+        if code == False:
+            bericht.showinfo("Toegangscode info", "Dit is geen bestaande toegangscode.")
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        menu = Menu(root)
+        root.config(menu=menu)
+
+        # ***** Main Menu *****
+
+        fileMenu = Menu(menu)
+        menu.add_cascade(label="File", menu=fileMenu)
+        fileMenu.add_command(label="New Project...", command=self.klik)
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", command=exit)
+
+
+
+
+        # ***** Toolbar *****
+
+        toolbar = Frame(root, bg="#A5110D")
+
+        bestelButton = Button(toolbar, text="Film bestellen", command=self.klik)
+        bestelButton.pack(side=LEFT, padx=5, pady=10)
+        bekijkButton = Button(toolbar, text="Film bekijken", command=self.code_checken)
+        bekijkButton.pack(side=LEFT, padx=5, pady=10)
+
+        toolbar.pack(side=TOP, fill=X)
+
+        # ***** Status Bar *****
+
+        self.status = Label(root, text="Statusbar...", bd=1, relief=SUNKEN, anchor=W)
+        self.status.pack(side=BOTTOM, fill=X)
+
     photo = PhotoImage(file="Studio100.png")
     label = Label(root, image=photo)
     label.pack(fill=X)
-    
-    frame = Frame(root, width=1280, height= 720, bg="#FFF")
-    frame.pack(fill=X)
-    
-    root.mainloop()
+
+
+
+startscherm(root)
+
+
+root.mainloop()
+
+
+
+
+
