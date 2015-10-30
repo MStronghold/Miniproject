caa17f42c9fe6972247012ddd6c406265863b183
MStronghold <deathisafriend93>
2015-10-30 01:24:16 +0100

    Gewerkt aan zoekfunctie. Werkend gekregen, dict from string naar JSON geswitched. Laten varen vanwege tekort tijd om te debuggen.

--- klad/Klad - Lucas.py
+++ klad/Klad - Lucas.py
@@ -1,45 +1,75 @@
+from urllib.request import urlopen      #http connector
+from urllib import error                #error handling
+from ast import literal_eval            #dict naar string converter
+import re                               #regular expressions voor url-opbouw en filtering
+import json
 
-# Library's
-import time
-import urllib.request
-import xml.etree.ElementTree as ET
 
-def API(x):
+def APIrequest(x, search=0):
     """
-    0=alle films
-    1=filmtips
-    2=film vd dag
+    Invoerfunctie voor zoekveld.
+    Accepteert string(titel of idnummer),
+    vraagt info en returned een dict met informatie.
+    :param x=str:
     :return:
     """
-    if type(x) is not int:
-        raise TypeError('API(x) x is not int')
-    elif x < 0 or x > 2:
-        raise AttributeError('API(x) Parameter out of bounds')
+
+    # vervangt spaties in zoektermen met '+' tekens
+    x = str(x)
+    x = re.sub(r'\s+', '+', x)
+
+    #bouwt API-geaccepteerde url
+    url = 'http://www.omdbapi.com/?'
+    if search==1:
+        url += 's=' + x
+        print(url)
+    elif x[:2] == 'tt':
+        url += 'i=' + x
     else:
-        url = 'http://www.filmtotaal.nl/api/filmsoptv.xml?apikey=b7w5qxk8dqtvbntnx5uns434fhww6878'
-        tijd = time.strftime('%d-%m-%Y')
-        url += '&dag=' + str(tijd)
-        url += '&sorteer=' + str(x)
-        try:
-            print(url)
-            return urllib.request.urlopen(url)
-        except:
-            return 'Kan geen verbinding maken.'
+        url += 't=' + x
 
-with open('lucasklad-bronlatin.txt', 'r') as infile:
-    with open('lucassklad-doelutf.txt', 'w+') as outfile:
-        bron = infile.read()
-        doel = bytes(bron, 'iso-8859-1').decode('utf-8')
-        outfile.write(doel)
+    #verbindt met API en extraheert gegevens, exceptions ingebouwd voor eventueel verbindingsverlies,
+    # server- en client-side
+    try:
+        with urlopen(url) as f:
+            data = f.read().decode()
+        # weergave raw data(debug)
+        print(data, '\n', '--', '\n')
+    except error.HTTPError as H:
+        print('server kan niet aan het verzoek voldoen.')
+        print('Foutcode: ', H.code)
+    except error.URLError as U:
+        print('kan server niet bereiken.')
+        print('Foutcode: ', U.code)
+
+    # print('data pre: ' + data)
+    # data = data.replace('{"Search":[', '')
+    # data = data.replace(']}', '')
+    print('data post: ' + str(type(data)) + '\n' + str(data))
+    #bouwt dictobject
+    json_string = data
+    gegevens = json.loads(json_string)
+    # gegevens[regel[0]] = regel[1]
+    # gegevens = literal_eval(data)
 
 
 
-# data = ET.parse(API(0))
-# with open('test.xml', 'w+') as bestand:
-#     for child in data.iter():
-#         datastring = ET.tostring(child).decode('iso-8859-1')
-#         datastring.encode('utf-8')
-#         bestand.write(str(datastring) + '\n')
+    #weergave dict(debug)
+    # for key in gegevens:
+    #     print(key, gegevens[key], '\n')
+    try:
+        if search==1:
+            print('Zoekresultaten:', '\n')
+            print(gegevens)
+        elif gegevens['Response'] == 'False':
+            APIrequest(x, search=1)
+            print('check tail', '\n')
+    except (KeyError):
+        return gegevens
+    return gegevens
 
 
-# datastring = ET.tostring(child).decode()
+
+
+#handmatige input(debug)
+APIrequest('star wars')
